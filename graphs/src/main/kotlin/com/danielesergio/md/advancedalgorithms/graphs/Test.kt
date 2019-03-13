645f572cad1bfa82c564ef1b35a71aabf6a45987
Daniele Sergio <daniele.sergio@kynetics.com>
2019-03-14 00:05:54 +0100

    Implement algorithm to calculate the connected component of a graph

--- graphs/src/main/kotlin/com/danielesergio/md/advancedalgorithms/graphs/Test.kt
+++ graphs/src/main/kotlin/com/danielesergio/md/advancedalgorithms/graphs/Test.kt
@@ -5,6 +5,7 @@ package com.danielesergio.md.advancedalgorithms.graphs
 import com.danielesergio.md.advancedalgorithms.graphs.model.Edge
 import com.danielesergio.md.advancedalgorithms.graphs.model.GraphBuilder
 import com.danielesergio.md.advancedalgorithms.graphs.model.GraphType
+import com.danielesergio.md.advancedalgorithms.graphs.questions.ConnectedComponentAlgorithm
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
@@ -22,32 +23,23 @@ fun main(args : Array<String>) {
         graph1.addEdge(it.first,it.second)
         graph2.addEdge(it.first,it.second)
     }
-    println("oriented: (${graph1.getEdges()})")
-    println("not oriented: (${graph2.getEdges()})")
+//    println("oriented: (${graph1.getEdges()})")
+//    println("not oriented: (${graph2.getEdges()})")
+//
+//    graph1.getVertices().forEach{
+//        println("g1) neighbour of $it (in,out) (${graph1.inNeighbours(it)}, ${graph1.outNeighbours(it)})")
+//        println("g2) neighbour of $it (in,out) (${graph2.inNeighbours(it)}, ${graph2.outNeighbours(it)})")
+//    }
 
-    graph1.getVertices().forEach{
-        println("g1) neighbour of $it (in,out) (${graph1.inNeighbours(it)}, ${graph1.outNeighbours(it)})")
-        println("g2) neighbour of $it (in,out) (${graph2.inNeighbours(it)}, ${graph2.outNeighbours(it)})")
-    }
+    val edgesCC = setOf(Edge(0,1), Edge(1,2), Edge(2,3), Edge(2,4), Edge(2,5), Edge(5,6), Edge(6,7), Edge(6,8), Edge(8,9),
+            Edge(10,11),Edge(12,11),Edge(12,13),Edge(10,13),
+            Edge(14,15),Edge(16,15))
 
+    val graph3 = GraphBuilder.newInstance(GraphType(selfLoopAllowed = false, oriented = false), (0 until 17).toMutableSet())
+    val cc = ConnectedComponentAlgorithm(graph3)
+    edgesCC.forEach{graph3.addEdge(it.first,it.second)}
+    println(cc.parse())
 
-//    generateGraphWithDPA(28000, 200)
-//    generateGraphWithDPA(28000, 28000)
-//    val graph1 = GraphBuilder.newUnDirectedGraphInstance(vertexSize, edges )
-//    val graph2 = GraphBuilder.newDirectedGraphInstance(vertexSize, edges )
-//    val graph2 = UnDirectedGraphAdjMatrix.newInstance(vertexSize, edges)
-//    println(graph1.vertexDistribution())
-//    println(graph2.inDegreeVertexDistribution())
-//    println(graph2.outDegreeVertexDistribution())
-//    println(graph2.conjunctDistri10bution()  )
-//    println("In degree")
-//    (0 until 10).forEach{println(graph2.inDegree(it))}
-//    println("out degree")
-//    (0 until 10).forEach{println(graph2.outDegree(it))}
-//    val graph3 = GraphBuilder.er(GraphType(selfLoopAllowed = false, oriented = true), 6474, 0.00032)
-//    println(graph3.getVertices().size)
-//    println(graph3.getEdges().size)
-//    println(graph2.vertexDistribution())
 }
 
 private fun generateGraphWithDPA(vertexSize:Int, m:Int) {
---
+++ graphs/src/main/kotlin/com/danielesergio/md/advancedalgorithms/graphs/questions/ConnectedComponentAlgorithm.kt
@@ -0,0 +1,46 @@
+package com.danielesergio.md.advancedalgorithms.graphs.questions
+
+import com.danielesergio.md.advancedalgorithms.graphs.model.Graph
+import java.lang.IllegalArgumentException
+
+/**
+ * @author Daniele Sergio
+ */
+class ConnectedComponentAlgorithm(private val graph: Graph) {
+
+    val vertexColors:MutableMap<Int,Color> = graph.getVertices().map{ it to Color.WHITE }.toMap().toMutableMap()
+
+
+    enum class Color{
+        WHITE, GREY, BLACK
+    }
+
+    fun parse():Set<Set<Int>>{
+        if(graph.type.oriented){
+            throw IllegalArgumentException("")
+        }
+
+        val CC = mutableSetOf<Set<Int>>()
+        graph.getVertices().forEach{v->
+            if(vertexColors[v] == Color.WHITE){
+                CC.add(dfs_visited(v))
+            }
+        }
+
+        return CC
+    }
+
+    private fun dfs_visited(u: Int):Set<Int>{
+        vertexColors[u] = Color.GREY
+        val visited = mutableSetOf(u)
+        graph.inNeighbours(u).forEach{ v ->
+            if(vertexColors[v] == Color.WHITE){
+                visited.addAll(dfs_visited(v))
+            }
+        }
+        vertexColors[u] = Color.BLACK
+        return visited
+    }
+
+}
+
