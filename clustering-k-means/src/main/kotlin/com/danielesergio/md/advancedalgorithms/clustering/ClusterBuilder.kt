6320b40aa037d2ed58884ee8b1e6738b5f580479
Daniele Sergio <daniele.sergio0@studenti.unipd.it>
2019-06-28 08:51:21 +0200

    Load data for 5 lab

--- clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/ClusterBuilder.kt
+++ clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/ClusterBuilder.kt
@@ -1,4 +1,43 @@
 package com.danielesergio.md.advancedalgorithms.clustering
 
+import java.util.zip.ZipFile
+
+data class Position(val lat:Double, val long:Double)
+data class City(val id:Int, val name:String, val population:Long, val position:Position)
+
 object ClusterBuilder {
+
+    /**
+     * fork join shoudl be equals
+     * http://bytes.schibsted.com/async-patterns-on-android/
+     */
+    private const val DATASET_RESOURCE = "cities-and-towns-of-usa"
+    private const val DATASET_RESOURCE_ZIP = "$DATASET_RESOURCE.zip"
+    private const val DATASET_RESOURCE_CSV_ENTRY = "$DATASET_RESOURCE.csv"
+    private val citiesMap : Map<Int, List<City>>
+    init{
+        val graphResourceFilePath = ClusterBuilder::class.java.classLoader.getResource(DATASET_RESOURCE_ZIP).file
+        val zipFile = ZipFile(graphResourceFilePath)
+
+        val cities = mutableListOf<City>()
+        zipFile.getInputStream(zipFile.getEntry(DATASET_RESOURCE_CSV_ENTRY))
+                .bufferedReader().lines()
+                .forEach{
+                    val data = it.split(",")
+                    cities.add(
+                            City(
+                                    id = data[0].toInt(),
+                                    name = data[1],
+                                    population = data[2].toLong(),
+                                    position = Position(
+                                            lat = data[3].toDouble(),
+                                            long = data[4].toDouble()
+                                    )
+                            )
+                    )
+                }
+        citiesMap = listOf(-1, 250, 2_000, 5_000, 15_000, 50_000, 100_000)
+                .map{ it to if(it < 1) cities else cities.filter{city -> city.population >= it} }
+                .toMap()
+    }
 }
