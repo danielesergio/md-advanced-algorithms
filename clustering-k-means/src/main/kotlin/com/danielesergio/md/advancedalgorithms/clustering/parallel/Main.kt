92ff6e8bf3170a4d613201cb8875e3bfd500f145
Daniele Sergio <daniele.sergio0@studenti.unipd.it>
2019-06-30 17:32:25 +0200

    Add question three

--- clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/parallel/Main.kt
+++ clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/parallel/Main.kt
@@ -22,6 +22,7 @@ fun main() {
     dir.mkdirs()
 
     Question.one(dir)
+    Question.two(dir)
 
 
 }
--- clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/parallel/Question.kt
+++ clustering-k-means/src/main/kotlin/com/danielesergio/md/advancedalgorithms/clustering/parallel/Question.kt
@@ -1,9 +1,6 @@
 package com.danielesergio.md.advancedalgorithms.clustering.parallel
 
 import com.danielesergio.md.advancedalgorithms.clustering.Algorithm
-import com.danielesergio.md.advancedalgorithms.clustering.ClusterBuilder.getDataSet
-import com.danielesergio.md.advancedalgorithms.clustering.model.Cluster
-import com.danielesergio.md.advancedalgorithms.clustering.model.County
 import java.io.File
 import java.time.Duration
 import java.time.Instant
@@ -17,7 +14,8 @@ import java.time.Instant
 object Question {
     const val GNUPLOT_FILE_NAME = "gnuplot_script"
 
-    const val PARALLEL_ALGO_NAME = "Parallel"
+    const val PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD = "Parallel_no_threshold"
+    const val PARALLEL_ALGO_NAME_WITH_THRESHOLD = "Parallel_with_threshold"
     const val SERIAL_ALGO_NAME = "Serial"
     const val ES4_ALGO_NAME = "Es4"
 
@@ -26,7 +24,8 @@ object Question {
         dir.mkdirs()
 
         val result = mutableMapOf<String, MutableSet<Pair<Int, Duration>>>(
-                PARALLEL_ALGO_NAME to mutableSetOf(),
+                PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD to mutableSetOf(),
+                PARALLEL_ALGO_NAME_WITH_THRESHOLD to mutableSetOf(),
                 SERIAL_ALGO_NAME to mutableSetOf(),
                 ES4_ALGO_NAME to mutableSetOf()
         )
@@ -37,8 +36,12 @@ object Question {
             val initialCenter = (0 until 50).map{ data.random().position}.toSet()
             val initialCenterArray = initialCenter.toTypedArray()
 
-            result[PARALLEL_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
+            result[PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
                 ParallelAlgorithm.kMeansClustering(data, initialCenterArray, 100, 0, 0)
+            }))
+
+            result[PARALLEL_ALGO_NAME_WITH_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClustering(data, initialCenterArray, 100, data.size, data.size)
             }))
 
             result[SERIAL_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
@@ -67,7 +70,116 @@ object Question {
                 set xlabel "Number of Points"
                 set autoscale xfix
                 set autoscale yfix
-                plot  "es1_$PARALLEL_ALGO_NAME.data" using 1:2 with lines lw 2 title '$PARALLEL_ALGO_NAME', "es1_$SERIAL_ALGO_NAME.data" using 1:2 with lines lw 2 title '$SERIAL_ALGO_NAME', "es1_$ES4_ALGO_NAME.data" using 1:2 with lines lw 2 title '$ES4_ALGO_NAME'
+                plot  "es1_$PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.split('_').joinToString(" ")}}', "es1_$PARALLEL_ALGO_NAME_WITH_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITH_THRESHOLD.split('_').joinToString (" ")}', "es1_$SERIAL_ALGO_NAME.data" using 1:2 with lines lw 2 title '$SERIAL_ALGO_NAME', "es1_$ES4_ALGO_NAME.data" using 1:2 with lines lw 2 title '$ES4_ALGO_NAME'
+        """.trimIndent())
+    }
+
+
+    fun two(dir: File){
+        val data = ClusterBuilderData.getCity(ClusterBuilderData.CityFilterByPop.ALL)
+
+        val result = mutableMapOf<String, MutableSet<Pair<Int, Duration>>>(
+                PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD to mutableSetOf(),
+                PARALLEL_ALGO_NAME_WITH_THRESHOLD to mutableSetOf(),
+                SERIAL_ALGO_NAME to mutableSetOf(),
+                ES4_ALGO_NAME to mutableSetOf()
+        )
+
+        (10 .. 100).forEach{ clusterSize ->
+
+            val initialCenter = (0 until clusterSize).map{ data.random().position}.toSet()
+            val initialCenterArray = initialCenter.toTypedArray()
+
+            result[PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClustering(data, initialCenterArray, 100, 0, 0)
+            }))
+
+            result[PARALLEL_ALGO_NAME_WITH_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClustering(data, initialCenterArray, 100, data.size, data.size)
+            }))
+
+            result[SERIAL_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClusteringSerial(data, initialCenterArray, 100)
+            }))
+
+            val dataAsSet = data.toSet()
+
+            result[ES4_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
+                Algorithm.kMeansClustering(dataAsSet, initialCenter, 100)
+            }))
+        }
+
+        result.forEach{
+            val file = File(dir, "es2_${it.key}.data")
+            it.value.forEach { data ->
+                file.appendText("${data.first}, ${data.second.toMillis()}\n")
+            }
+        }
+
+        val gnuPlotFile = File(dir, GNUPLOT_FILE_NAME)
+        gnuPlotFile.appendText("""
+            
+                set terminal png size 800,600
+                set output 'question_2.png'
+                set ylabel "Execution time (in milliseconds)"
+                set xlabel "Number of clusters"
+                set autoscale xfix
+                set autoscale yfix
+                plot  "es2_$PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.split('_').joinToString(" ")}',"es2_$PARALLEL_ALGO_NAME_WITH_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITH_THRESHOLD.split('_').joinToString(" ")}',"es2_$SERIAL_ALGO_NAME.data" using 1:2 with lines lw 2 title '$SERIAL_ALGO_NAME', "es2_$ES4_ALGO_NAME.data" using 1:2 with lines lw 2 title '$ES4_ALGO_NAME'
+        """.trimIndent())
+    }
+
+    fun three(dir: File){
+        val data = ClusterBuilderData.getCity(ClusterBuilderData.CityFilterByPop.ALL)
+
+        val result = mutableMapOf<String, MutableSet<Pair<Int, Duration>>>(
+                PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD to mutableSetOf(),
+                PARALLEL_ALGO_NAME_WITH_THRESHOLD to mutableSetOf(),
+                SERIAL_ALGO_NAME to mutableSetOf(),
+                ES4_ALGO_NAME to mutableSetOf()
+        )
+
+        (10 .. 1000).forEach{ iter ->
+
+            val initialCenter = (0 until 50).map{ data.random().position}.toSet()
+            val initialCenterArray = initialCenter.toTypedArray()
+
+            result[PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClustering(data, initialCenterArray, iter, 0, 0)
+            }))
+
+            result[PARALLEL_ALGO_NAME_WITH_THRESHOLD]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClustering(data, initialCenterArray, iter, data.size, data.size)
+            }))
+
+            result[SERIAL_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
+                ParallelAlgorithm.kMeansClusteringSerial(data, initialCenterArray, iter)
+            }))
+
+            val dataAsSet = data.toSet()
+
+            result[ES4_ALGO_NAME]!!.add(Pair(data.size, getExecutionTime {
+                Algorithm.kMeansClustering(dataAsSet, initialCenter, iter)
+            }))
+        }
+
+        result.forEach{
+            val file = File(dir, "es3_${it.key}.data")
+            it.value.forEach { data ->
+                file.appendText("${data.first}, ${data.second.toMillis()}\n")
+            }
+        }
+
+        val gnuPlotFile = File(dir, GNUPLOT_FILE_NAME)
+        gnuPlotFile.appendText("""
+            
+                set terminal png size 800,600
+                set output 'question_3.png'
+                set ylabel "Execution time (in milliseconds)"
+                set xlabel "Number of iterations"
+                set autoscale xfix
+                set autoscale yfix
+                plot  "es3_$PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITHOUT_THRESHOLD.split('_').joinToString(" ")}',"es3_$PARALLEL_ALGO_NAME_WITH_THRESHOLD.data" using 1:2 with lines lw 2 title '${PARALLEL_ALGO_NAME_WITH_THRESHOLD.split('_').joinToString(" ")}',"es3_$SERIAL_ALGO_NAME.data" using 1:2 with lines lw 2 title '$SERIAL_ALGO_NAME', "es3_$ES4_ALGO_NAME.data" using 1:2 with lines lw 2 title '$ES4_ALGO_NAME'
         """.trimIndent())
     }
 
